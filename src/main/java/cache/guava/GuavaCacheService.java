92e27e8b6cd0a83965d937ee64b6a0a82020b677
lilong <lilong@corp.netease.com>
2019-03-14 22:15:04 +0800

    guava cache

--- src/main/java/cache/guava/GuavaCacheService.java
+++ src/main/java/cache/guava/GuavaCacheService.java
@@ -15,46 +15,41 @@ import java.util.concurrent.TimeUnit;
  */
 public class GuavaCacheService {
     private static LoadingCache<String, String> localCache = CacheBuilder.newBuilder()
-            .maximumSize(5)//缓存容量设为5，实际上只用到1个
-//            .refreshAfterWrite(3, TimeUnit.SECONDS)//设置定时刷新
-            .expireAfterWrite(1, TimeUnit.SECONDS)//如果数据长时间没有被访问，则强制刷新
+            .maximumSize(5) // 缓存容量设为5，实际上只用到1个
+            .refreshAfterWrite(3, TimeUnit.SECONDS) // 一个线程load，其他线程返回旧值
+//            .expireAfterWrite(3, TimeUnit.SECONDS) // 一个线程load，其他线程阻塞等待
             .removalListener((removalNotification) -> // 匿名内部类RemovalListener
-                System.out.println(Thread.currentThread().getId() + ":移除key：" + removalNotification.getKey())
+                System.out.println(Thread.currentThread().getName() + ":已移除")
             )
             .build(new CacheLoader<String, String>() {
                 @Override
                 public String load(String key) throws InterruptedException {
-                    System.out.println(Thread.currentThread().getId() + ":开始移除key");
+                    System.out.println(Thread.currentThread().getName() + ":开始移除key..." + key);
                     Thread.sleep(5000);
                     LocalDateTime dateTime = LocalDateTime.now();
                     String time = dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-                    System.out.println(Thread.currentThread().getId() + ":移除成功");
+                    System.out.println(Thread.currentThread().getName() + ":移除成功key..." + key);
                     return "key_" + key + "_" + time;
                 }
             });
 
-    public static void main(String[] args) throws Exception {
-        new Thread(()->{
+    public static void main(String[] args) {
+        new Thread(new GetCache(), "Thread-1").start();
+        new Thread(new GetCache(), "Thread-2").start();
+    }
+
+    static class GetCache implements Runnable {
+        @Override
+        public void run() {
             while (true) {
-                String val = null;
                 try {
-                    val = localCache.get("a");
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(Thread.currentThread().getId() + ":" +  val);
-                try {
+                    String val = localCache.get("a");
+                    System.out.println(Thread.currentThread().getName() + ":" + val);
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        }).start();
-
-        while (true) {
-            String val = localCache.get("a");
-            System.out.println(Thread.currentThread().getId() + ":" + val);
-            Thread.sleep(1000);
         }
     }
 }
