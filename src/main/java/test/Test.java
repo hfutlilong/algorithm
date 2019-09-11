af0ec9c7b29c9188dc41e95c5890df43a3990fa8
lilong <lilong@corp.netease.com>
2019-09-11 10:35:09 +0800

    Dubbo SPI

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,13 +1,47 @@
 package test;
 
+import javassist.*;
+import org.apache.dubbo.common.extension.ExtensionLoader;
 import test.spi.Animal;
 
+import java.lang.reflect.Method;
 import java.util.ServiceLoader;
 
 public class Test {
     public static void main(String[] args) {
+//        testJavaSPI();
+        testDubboSPI();
+
+//        testJavassist();
+    }
+
+    private static void testJavaSPI() {
         ServiceLoader<Animal> serviceLoader = ServiceLoader.load(Animal.class);
-        System.out.println("Java SPI TEST");
         serviceLoader.forEach(Animal::sayHello);
+    }
+
+    private static void testDubboSPI() {
+        ExtensionLoader<Animal> extensionLoader =
+                ExtensionLoader.getExtensionLoader(Animal.class);
+        Animal dog = extensionLoader.getExtension("dog");
+        dog.sayHello();
+        Animal cat = extensionLoader.getExtension("cat");
+        cat.sayHello();
+    }
+
+    private static void testJavassist() {
+        try {
+            ClassPool classPool = ClassPool.getDefault();
+            CtClass ctClass = classPool.makeClass("HelloWorld");
+            CtMethod ctMethod = CtNewMethod.make("public static void test(){System.out.println(\"Hello World!\");}", ctClass);
+            ctClass.addMethod(ctMethod);
+            Class aClass = ctClass.toClass();
+
+            Object object = aClass.newInstance();
+            Method m = aClass.getDeclaredMethod("test", null);
+            m.invoke(object, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/test/spi/Animal.java
+++ src/main/java/test/spi/Animal.java
@@ -1,5 +1,8 @@
 package test.spi;
 
+import org.apache.dubbo.common.extension.SPI;
+
+@SPI
 public interface Animal {
     void sayHello();
 }
